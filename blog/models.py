eece8bae9c89833f34e524142d79b86235f2a716
Maksim <felozan4ik2007@gmail.com>
2025-05-29 00:16:52 +0300

    comments

--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,13 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author_name = models.CharField(max_length=50)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author_name} on {self.post.title}'
