1c4e2c5db871a33703e8f2ea50c502bde48620b4
Maksim <felozan4ik2007@gmail.com>
2025-05-28 23:27:09 +0300

    author added

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,15 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return self.title
+    
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
